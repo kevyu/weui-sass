ba47ca3ac70e89e5dd7a505ea5bb3e4cddff557f
wuweihui <52500925@qq.com>
2016-05-12 19:49:20 +0800

    1.同步weui官网demo； 2.新增首页4个图标和1个文章图片； 3.将gulp配置中的gulp-minify-css修改为gulp-clean-css; 4.新增命名空间${ns}:weui-(这部分我还没全修复，可以的话更方便项目使用)； 5.修复weui_media_box中sass不支持Changing Selector Order语法； 6.修复weui_btn_title中被忽视的margin_bottom; PS： 1.这个UI公司项目也在用，所以我也想出份力啊！ 2.关于命名空间如果您同意的话，那我就大胆将所有weui_改成#{ns}了啊，栗子:weui_media_box.scss 3.还有一个问题就是那个hashchange只能翻一次页呢，所以tab那个页面就有点尴尬了

--- src/example/example.js
+++ src/example/example.js
@@ -80,6 +80,36 @@ $(function () {
         });
     });
 
+    $container.on('click', '.weui_navbar_item,.weui_tabbar_item', function () {
+        $(this).addClass('weui_bar_item_on').siblings().removeClass('weui_bar_item_on');
+    });
+
+    $container.on('focus', '#search_input', function () {
+        var $weuiSearchBar = $('#search_bar');
+        $weuiSearchBar.addClass('weui_search_focusing');
+    }).on('blur', '#search_input', function () {
+        var $weuiSearchBar = $('#search_bar');
+        $weuiSearchBar.removeClass('weui_search_focusing');
+        if ($(this).val()) {
+            $('#search_text').hide();
+        } else {
+            $('#search_text').show();
+        }
+    }).on('input', '#search_input', function () {
+        var $searchShow = $("#search_show");
+        if ($(this).val()) {
+            $searchShow.show();
+        } else {
+            $searchShow.hide();
+        }
+    }).on('touchend', '#search_cancel', function () {
+        $("#search_show").hide();
+        $('#search_input').val('');
+    }).on('touchend', '#search_clear', function () {
+        $("#search_show").hide();
+        $('#search_input').val('');
+    });
+
     function hideActionSheet(weuiActionsheet, mask) {
         weuiActionsheet.removeClass('weui_actionsheet_toggle');
         mask.removeClass('weui_fade_toggle');
